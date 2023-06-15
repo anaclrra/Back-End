7a36f754cfdccff4f320c49ba3766ae53f9b4d04
Ana Clara <clara.bonifacio@escolar.ifrn.edu.br>
2023-06-15 08:35:31 -0300

    implementando método de login

--- exercicio_backend/app.js
+++ exercicio_backend/app.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const mysql = require('mysql');
+const jwt = require('jsonwebtoken');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
 const app = express();
+app.use(cors());
+app.use(bodyParser.json());
 
 const connection = mysql.createConnection({
     host: 'localhost',
@@ -37,6 +43,33 @@ app.get('/users/:id', (req,res) => {
         }
         else {
             res.json(rows[0]);
+        }        
+    });
+});
+
+function gerarToken(payload) {
+    const senhaToken = 'ana2@05';
+    return jwt.sign(payload, senhaToken, {expiresIn: 20});
+};
+
+app.post('/login', (req,res) => {
+    const loginname = req.body.loginname;
+    const password = req.body.password;
+    connection.query('SELECT nomeUsuario FROM usuarios WHERE loginName = ? AND password = ?', 
+    [loginname, password], (error, rows) => {
+        if(error) {
+            console.log('Erro ao processar o comando SQL.', )
+        }
+        else {
+            if(rows.length > 0) {
+                const payload = {noUsuario: rows[0].nomeUsuario};
+                const token = gerarToken(payload);
+                res.json({ acessToken: token});
+            }
+            else {
+                res.status(403).json({ messageErro: 'Login Inválido!' });
+            }
+            
         }        
     });
 });
